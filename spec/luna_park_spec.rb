d1d6dcc1843937c263432778de09e3da5a8c8f48
Captain Philipp <ph-s@mail.ru>
2018-11-02 15:14:36 +0300

    Entity spec

--- spec/luna_park_spec.rb
+++ spec/luna_park_spec.rb
@@ -1,9 +1,7 @@
+# frozen_string_literal: true
+
 RSpec.describe LunaPark do
   it 'has a version number' do
     expect(LunaPark::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
   end
 end
