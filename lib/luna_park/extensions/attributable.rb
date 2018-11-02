ef2aff952cd86828edb7d745d58b8da54d03f0b6
Captain Philipp <ph-s@mail.ru>
2018-11-02 16:07:54 +0300

    Public version of set_attributes

--- lib/luna_park/extensions/attributable.rb
+++ lib/luna_park/extensions/attributable.rb
@@ -3,17 +3,39 @@
 module LunaPark
   module Extensions
     module Attributable
-      def set_attributes(hash)
-        unless hash.is_a?(Hash)
-          raise TypeError, "Expected to receive Hash, but receives #{hash.class} #{hash.inspect}"
+      private
+
+      def public_set_attributes(hash)
+        __guard_hash(hash)
+
+        __rename_no_method_exception do
+          hash.each { |k, v| public_send(:"#{k}=", v) }
         end
+      end
 
-        hash.each { |method_name, value| send(:"#{method_name}=", value) }
+      def set_attributes(hash)
+        __guard_hash(hash)
 
-      rescue NameError => e
-        method = e.message[/\Aundefined local variable or method `(.+)=' for/]
-        raise NameError, "Unknown attribute #{method} given. \n" \
-                         "Maybe you forgot to add `attr_accessor :#{method}`?"
+        __rename_no_method_exception do
+          hash.each { |k, v| send(:"#{k}=", v) }
+        end
+      end
+
+      def __guard_hash(hash)
+        return if hash.is_a?(Hash)
+
+        raise TypeError, "Expected to receive Hash, but receives #{hash.class} #{hash.inspect}"
+      end
+
+      def __rename_no_method_exception
+        yield
+        rescue NameError => e
+          method = e.message[/\Aundefined local variable or method `(.+)=' for/]
+
+          raise if method.nil?
+
+          raise NameError, "Unknown attribute #{method} given. \n" \
+                           "Maybe you forgot to add `attr_accessor :#{method}`?"
       end
     end
   end
