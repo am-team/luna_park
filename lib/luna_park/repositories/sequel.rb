afcb4ba1f154e307fde63eefbe006922b7c93072
Captain Philipp <ph-s@mail.ru>
2019-04-02 20:03:02 +0300

    Draft Repositories::Sequel

---
+++ lib/luna_park/repositories/sequel.rb
@@ -0,0 +1,89 @@
+# frozen_string_literal: true
+
+module LunaPark
+  module Repositories
+    # @example
+    #   class UserRepo < LunaPark::Repositories::Sequel
+    #     mapper UserMapper
+    #     entity User
+    #
+    #     def find(id)
+    #       to_entity from_row dataset.where(id: id).first
+    #     end
+    #
+    #     def create(input)
+    #       entity = wrap(input)
+    #       row = to_row(entity)
+    #       new_row = dataset.returning.insert(row)
+    #       new_attrs = from_row(new_attrs)
+    #       entity.set_attributes(new_attrs)
+    #       entity
+    #     end
+    #
+    #     # same as previous method `create`, but using helper
+    #     def save(wrappable)
+    #       modify_entity(wrappable) { |entity, row| dataset.returning.where(id: entity.id).update(row) }
+    #     end
+    #
+    #     def dataset
+    #       SEQUEL_DB[:users]
+    #     end
+    #   end
+    class Sequel
+      class << self
+        def mapper(mapper_klass)
+          @mapper_klass = mapper_klass
+        end
+
+        def entity(entity_class)
+          @entity_class = entity_class
+        end
+
+        def modify_entity(input)
+          entity = wrap(input)
+          row = to_row(entity)
+
+          new_row = yield(entity, row)
+
+          new_attrs = from_row(new_row)
+          entity.set_attributes_from_source(new_attrs)
+          entity
+        end
+
+        def to_rows(input_array)
+          mapper_klass&.to_rows(input_array)
+        end
+
+        def to_row(input)
+          mapper_klass&.to_row(input)
+        end
+
+        def from_rows(rows_array)
+          mapper_klass&.from_rows(rows_array)
+        end
+
+        def from_row(row)
+          mapper_klass&.from_row(row)
+        end
+
+        def wrap_all(input_array)
+          Array(input_array).map { |input| wrap(input) }
+        end
+
+        def wrap(input)
+          entity_class.wrap(input)
+        end
+
+        def to_entities(attrs_array)
+          Array(attrs_array).map { |attrs| to_entity(attrs) }
+        end
+
+        def to_entity(attrs)
+          entity_class.new(attrs)
+        end
+
+        attr_reader :mapper_klass, :entity_class
+      end
+    end
+  end
+end
