d93246d2bf5c5106ee666373065ffc6f3ed262e0
Captain Philipp <ph-s@mail.ru>
2019-06-18 19:50:10 +0300

    Add dependency setters and allow dependency without default

--- lib/luna_park/extensions/dependency_injectable.rb
+++ lib/luna_park/extensions/dependency_injectable.rb
@@ -11,50 +11,75 @@ module LunaPark
     #   class CreateTransaction
     #     include LunaPark::Extensions::DependencyInjectable
     #
-    #     dependency(:repository) { Repository.new }
-    #     dependency :today,      -> { Date.today }
-    #     dependency :notifier,   Notifier, call: false
+    #     dependency :observer                        # dependency without default value
+    #     dependency(:repository) { Repository.new }  # callable dependency as block (will be called every time) as block
+    #     dependency :time, -> { Time.now }           # callable dependency as callable object (will be called every time)
+    #     dependency :notifier, Notifier, call: false # dependency with default static object
     #
     #     def call
-    #       [repository, today, notifier]
+    #       [repository, time, notifier]
     #     end
     #   end
     #
     #   service = CreateTransaction.new(attrs)
-    #   service.repository              # => raised "private method called"
+    #   service.repository              # => raise "private method called"
     #   service.send(:repository)       # => #<Repository ...>
     #   service.dependencies.repository # => #<Repository ...>
     #   service.call                    # => [#<Repository ...>, #<Date ...>, Notifier]
     #
-    #   service.dependencies = OpenStruct.new(repository: 'REPO', today: 'TODAY')
-    #   service.call # => ["REPO", "TODAY", nil]
+    #   service.dependencies.time = 'NOW'
+    #   service.call # => ["REPO", "NOW", nil]
+    #
+    #   service.dependencies = OpenStruct.new(repository: 'REPO', time: 'YESTERDAY')
+    #   service.call # => ["REPO", "YESTERDAY", nil]
     module DependencyInjectable
       def self.included(base)
         base.extend  ClassMethods
         base.include InstanceMethods
       end
 
-      # DI Container Mixin
-      module SimpleContainer
-        def register(name, value = nil, call: true, &block)
+      # DI Container
+      class SimpleContainer
+        # @example
+        # class MyContainer < SimpleContainer
+        #   dependency(:foo)   { 'Foo' }
+        #   dependency :bar, ->{ 'Bar' }
+        #   dependency :baz,     'Baz', call: false
+        #   dependency :bat
+        # end
+        #
+        # container = MyContainer.new
+        # container.foo #= > "Foo"
+        # container.bar #= > "Bar"
+        # container.baz #= > "Baz"
+        # container.bat #= >  nil
+        # container.bat = 'Bat'
+        # container.bat #= > "Bat"
+        def self.dependency(getter, value = nil, call: true, &block) # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/MethodLength, Metrics/PerceivedComplexity
+          ivar   = :"@#{getter}"
+          setter = :"#{getter}="
+
           if block_given?
-            call ? define_method(name, &block) : define_method(name) { block }
+            define_method(getter) { block.call || instance_variable_get(ivar) }
+            define_method(setter) { |input| instance_variable_set(ivar, input) }
+          elsif value && call
+            define_method(getter) { value.call || instance_variable_get(ivar) }
+            define_method(setter) { |input| instance_variable_set(ivar, input) }
           elsif value
-            call ? define_method(name, &value) : define_method(name) { value }
-          else
-            raise ArgumentError, 'Expected value or block'
+            define_method(getter) { value || instance_variable_get(ivar) }
+            define_method(setter) { |input| instance_variable_set(ivar, input) }
           end
         end
       end
 
       module ClassMethods
         def dependency(name, value = nil, call: true, &block)
-          dependencies.register(name, value, call: call, &block)
+          dependencies.dependency(name, value, call: call, &block)
           private(define_method(name) { dependencies.public_send(name) })
         end
 
         def dependencies
-          @dependencies ||= Class.new { extend SimpleContainer }
+          @dependencies ||= Class.new(SimpleContainer)
         end
       end
 
