9df36e8a03bdf0e7c8ba385bcd6b8704bb77055b
Captain Philipp <ph-s@mail.ru>
2020-07-25 23:29:47 +0300

    Improve Substitutive: allow message, args and options

--- lib/luna_park/extensions/exceptions/substitutive.rb
+++ lib/luna_park/extensions/exceptions/substitutive.rb
@@ -5,31 +5,38 @@ module LunaPark
     module Exceptions
       # class-level mixin
       # @example
-      #   class MyException < StandardError
+      #   ##############################
+      #   # BAD (without Substitutive) #
+      #   ##############################
+      #
+      #   class CustomException < StandardError; end
+      #
+      #   begin
+      #     use_exceptional_gem!
+      #   rescue ExceptionFromGem => ex
+      #     raise CustomException
+      #   end
+      #
+      #   # Raised exception has backtrace started from `raise CustomException`
+      #   # So it's not containing the origin backtrace, that can be very painfull for debug
+      #
+      # @example
+      #   ############################
+      #   # GOOD (with Substitutive) #
+      #   ############################
+      #
+      #   class CustomException < StandardError
       #     extend LunaPark::Extensions::Exceptions::Substitutive
       #   end
       #
-      #   ############
-      #   # BAD CASE #
-      #   ############
       #   begin
-      #     call_exceptional_lib!
-      #   rescue ExceptionalLib::SomeException => e
-      #     raise MyException # => raised MyException with backtrace started from `raise MyException`
-      #                       #      and not contained origin exception backtrace
-      #                       #      that can be very painfull for debug
+      #     use_exceptional_gem!
+      #   rescue ExceptionFromGem => ex
+      #     raise CustomException.substitute(ex, "Oh! That's bad!", custom_data: ex.detail)
       #   end
       #
-      #   ###########
-      #   # RESOLVE #
-      #   ###########
-      #   begin
-      #     call_exceptional_lib!
-      #   rescue ExceptionalLib::SomeException => e
-      #     raise MyException.substitute(e) # => raised MyException with backtrace started
-      #                                     #      from library `raise ExceptionalLib::SomeException`
-      #                                     #      so you can easily find out where exception starts
-      #   end
+      #   # Raised exception has backtrace started from origin backtrace of ExceptionFromGem
+      #   # So you can easily find out where was the exception started
       module Substitutive
         def self.extended(base)
           base.extend  ClassMethods
@@ -37,17 +44,36 @@ module LunaPark
         end
 
         module ClassMethods
-          def substitute(origin)
-            new = new(origin.message)
-            new.backtrace = origin.backtrace
-            new.origin    = origin
-            new
+          def substitute(origin, message = nil, *args, **opts)
+            if __initializer_has_named_args?
+              new(message, *args, **opts).substitute!(origin)
+            else
+              new(message, *args).substitute!(origin)
+            end
+          end
+
+          private
+
+          def __initializer_has_named_args?
+            return @__initializer_has_named_args unless @__initializer_has_named_args.nil?
+
+            signature      = instance_method(:initialize).parameters
+            arg_types      = signature.map!(&:first)
+            has_named_args = arg_types.any? { |arg_type| %i[keyreq key keyrest].include?(arg_type) }
+
+            @__initializer_has_named_args = has_named_args
           end
         end
 
         module InstanceMethods
           attr_accessor :origin
           attr_writer :backtrace
+
+          def substitute!(origin)
+            self.backtrace = origin.backtrace
+            self.origin    = origin
+            self
+          end
 
           def backtrace
             super || @backtrace
