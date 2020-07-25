ca6d31971d719eebd19687a1424b45ebc0bb1f62
Captain Philipp <ph-s@mail.ru>
2020-07-25 23:29:47 +0300

    Improve Substitutive

--- lib/luna_park/extensions/exceptions/substitutive.rb
+++ lib/luna_park/extensions/exceptions/substitutive.rb
@@ -4,7 +4,39 @@ module LunaPark
   module Extensions
     module Exceptions
       # class-level mixin
-
+      # @example
+      #   ##############################
+      #   # BAD (without Substitutive) #
+      #   ##############################
+      #
+      #   class CustomException < StandardError; end
+      #
+      #   begin
+      #     use_exceptional_gem!
+      #   rescue ExceptionFromGem => ex
+      #     raise CustomException
+      #   end
+      #
+      #   # Raised exception has backtrace started from `raise CustomException`
+      #   # So it's not containing the origin backtrace, that can be very painfull for debug
+      #
+      # @example
+      #   ############################
+      #   # GOOD (with Substitutive) #
+      #   ############################
+      #
+      #   class CustomException < StandardError
+      #     extend LunaPark::Extensions::Exceptions::Substitutive
+      #   end
+      #
+      #   begin
+      #     use_exceptional_gem!
+      #   rescue ExceptionFromGem => ex
+      #     raise CustomException.substitute(ex, "Oh! That's bad!", custom_data: ex.detail)
+      #   end
+      #
+      #   # Raised exception has backtrace started from origin backtrace of ExceptionFromGem
+      #   # So you can easily find out where was the exception started
       module Substitutive
         def self.extended(base)
           base.extend  ClassMethods
@@ -12,39 +44,36 @@ module LunaPark
         end
 
         module ClassMethods
-          # Substitute original exception with save original backtrace.
-          #
-          # @example bad case
-          #   class MyException < StandardError
-          #     extend LunaPark::Extensions::Exceptions::Substitutive
-          #   end
-          #
-          #     call_exceptional_lib!
-          #   rescue ExceptionalLib::SomeException => e
-          #     raise MyException # => raised MyException with backtrace started from `raise MyException`
-          #                       #      and not contained origin exception backtrace
-          #                       #      that can be very painfull for debug
-          #   end
-          #
-          # @example resolve
-          #   begin
-          #     call_exceptional_lib!
-          #   rescue ExceptionalLib::SomeException => e
-          #     raise MyException.substitute(e) # => raised MyException with backtrace started
-          #                                     #      from library `raise ExceptionalLib::SomeException`
-          #                                     #      so you can easily find out where exception starts
-          #   end
-          def substitute(origin)
-            new = new(origin.message)
-            new.backtrace = origin.backtrace
-            new.origin    = origin
-            new
+          def substitute(origin, message = nil, *args, **opts)
+            if __initializer_has_named_args?
+              new(message, *args, **opts).substitute!(origin)
+            else
+              new(message, *args).substitute!(origin)
+            end
+          end
+
+          private
+
+          def __initializer_has_named_args?
+            return @__initializer_has_named_args unless @__initializer_has_named_args.nil?
+
+            signature      = instance_method(:initialize).parameters
+            arg_types      = signature.map!(&:first)
+            has_named_args = arg_types.any? { |arg_type| %i[keyreq key keyrest].include?(arg_type) }
+
+            @__initializer_has_named_args = has_named_args
           end
         end
 
         module InstanceMethods
           attr_accessor :origin
           attr_writer :backtrace
+
+          def substitute!(origin)
+            self.backtrace = origin.backtrace
+            self.origin    = origin
+            self
+          end
 
           def backtrace
             super || @backtrace
--- spec/luna_park/extensions/exceptions/substitutive_spec.rb
+++ spec/luna_park/extensions/exceptions/substitutive_spec.rb
@@ -3,23 +3,41 @@
 require 'luna_park/extensions/exceptions/substitutive'
 
 module ExtensionsExceptionsSubstitutiveSpec
-  class RegularError < StandardError; end
-
   class SubstitutiveError < StandardError
     extend LunaPark::Extensions::Exceptions::Substitutive
   end
 
+  class SubstitutiveWithArgs < StandardError
+    extend LunaPark::Extensions::Exceptions::Substitutive
+
+    attr_reader :comment
+
+    def initialize(msg = nil, comment = nil)
+      @comment = comment
+      super(msg)
+    end
+  end
+
+  class SubstitutiveWithOptsAndBuiltMessage < StandardError
+    extend LunaPark::Extensions::Exceptions::Substitutive
+
+    attr_reader :comment
+
+    def initialize(msg = nil, comment: nil)
+      @comment = comment
+      super(msg || build_message)
+    end
+
+    def build_message
+      "Comment: #{comment.inspect}"
+    end
+  end
+
   class << self
-    ORIGIN_EXCEPTION = StandardError.new('Foo')
+    ORIGIN_EXCEPTION = StandardError.new('OriginMsg')
 
     def raise_origin_exception
       raise ORIGIN_EXCEPTION
-    end
-
-    def raise_replaced_exception
-      raise_origin_exception
-    rescue StandardError
-      raise RegularError
     end
 
     def raise_substitutive_exception
@@ -27,58 +45,87 @@ module ExtensionsExceptionsSubstitutiveSpec
     rescue StandardError => e
       raise SubstitutiveError.substitute(e)
     end
+
+    def raise_substitutive_with_msg
+      raise_origin_exception
+    rescue StandardError => e
+      raise SubstitutiveError.substitute(e, 'NewMsg')
+    end
+
+    def raise_substitutive_with_args
+      raise_origin_exception
+    rescue StandardError => e
+      raise SubstitutiveWithArgs.substitute(e, 'NewMsg', 'Oy vey!')
+    end
+
+    def raise_substitutive_with_opts
+      raise_origin_exception
+    rescue StandardError => e
+      raise SubstitutiveWithOptsAndBuiltMessage.substitute(e, comment: 'Oy vey!')
+    end
   end
 end
 
 module LunaPark
   RSpec.describe Extensions::Exceptions::Substitutive do
-    let(:origin_exception)      { catch { ExtensionsExceptionsSubstitutiveSpec.raise_origin_exception } }
-    let(:replaced_exception)    { catch { ExtensionsExceptionsSubstitutiveSpec.raise_replaced_exception } }
-    let(:substituted_exception) { catch { ExtensionsExceptionsSubstitutiveSpec.raise_substitutive_exception } }
+    let(:origin_exception)      { exception { ExtensionsExceptionsSubstitutiveSpec.raise_origin_exception } }
 
-    describe 'substitutive exception' do
-      it 'has name of origin exception' do
-        expect(substituted_exception.message).to be origin_exception.message
-      end
+    context 'with any args,' do
+      let(:substituted_exception) { exception { ExtensionsExceptionsSubstitutiveSpec.raise_substitutive_exception } }
 
-      it 'backtrace starts from origin exception backtrace' do
+      it 'backtrace starts from the origin exception backtrace' do
         expect(substituted_exception.backtrace.first).to be origin_exception.backtrace.first
       end
 
-      it 'includes backtrace of origin exception' do
+      it 'includes backtrace of the origin exception' do
         expect(substituted_exception.backtrace.map { |p| p.split(':').last }).to include 'in `raise_origin_exception\''
       end
 
-      it 'includes backtrace of new exception' do
+      it 'includes backtrace of the substitutive exception' do
         expect(substituted_exception.backtrace.map { |p| p.split(':').last }).to include 'in `raise_substitutive_exception\''
       end
 
-      it 'contains origin exception object' do
+      it 'contains the origin exception object' do
         expect(substituted_exception.origin).to be origin_exception
       end
     end
 
-    describe 'replaced exception' do
-      it 'not includes backtrace of origin exception' do
-        expect(replaced_exception.backtrace).not_to include origin_exception.backtrace.first
+    context 'when substituted with new message,' do
+      let(:substituted_exception) { exception { ExtensionsExceptionsSubstitutiveSpec.raise_substitutive_with_msg } }
+
+      it 'has added message' do
+        expect(substituted_exception.message).to eq 'NewMsg'
       end
     end
 
-    describe '#cover_up_backtrace' do
-      it 'should get original backtrace' do
-        begin
-          original_error = ExtensionsExceptionsSubstitutiveSpec::SubstitutiveError.new('Something went wrong')
-          raise original_error
-        rescue StandardError => e
-          raise e.cover_up_backtrace
-        end
-      rescue StandardError => e
-        expect(e.backtrace).to eq original_error.backtrace
+    context 'when substituted with additional args,' do
+      let(:substituted_exception) { exception { ExtensionsExceptionsSubstitutiveSpec.raise_substitutive_with_args } }
+
+      it 'has added message' do
+        expect(substituted_exception.message).to eq 'NewMsg'
+      end
+
+      it 'additional named args was performed' do
+        expect(substituted_exception.comment).to eq 'Oy vey!'
       end
     end
 
-    def catch
+
+    context 'when substituted with additional named args AND built message,' do
+      let(:substituted_exception) { exception { ExtensionsExceptionsSubstitutiveSpec.raise_substitutive_with_opts } }
+
+      it 'has built message' do
+        expect(substituted_exception.message).to eq 'Comment: "Oy vey!"'
+      end
+
+      it 'additional named args was performed' do
+        expect(substituted_exception.comment).to eq 'Oy vey!'
+      end
+    end
+
+    def exception
       yield
+      nil
     rescue => e # rubocop:disable Style/RescueStandardError:
       e
     end
