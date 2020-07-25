224127aff472fcb8e99bab401a82cadd785d2621
Captain Philipp <ph-s@mail.ru>
2020-07-25 23:50:07 +0300

    Minor: rename local vars

--- lib/luna_park/mappers/codirectional/copiysts/symmetric.rb
+++ lib/luna_park/mappers/codirectional/copiysts/symmetric.rb
@@ -9,20 +9,20 @@ module LunaPark
         # Copyist for copiyng value between two schemas with SAME NESTING PATHS
         #   (Works with Array of described attributes)
         class Symmetric < Abstract
-          # @keypaths is paths like [[:charge, :amount], [:charge, :currency]]
-          # @keys     is keys  like [:id, :comment]
-          def initialize(keys)
-            @keypaths, @keys = keys.partition { |key| key.is_a?(Array) }
+          # @nested_keys are paths like [[:charge, :amount], [:charge, :currency]]
+          # @plain_keys  are keys  like [:id, :comment]
+          def initialize(paths)
+            @nested_keys, @plain_keys = paths.partition { |path| path.is_a?(Array) }
           end
 
           def from_row(row:, attrs:)
-            attrs.merge! row.slice(*@keys)
-            @keypaths.each { |path| copy_nested(from: row, to: attrs, from_path: path, to_path: path) }
+            attrs.merge! row.slice(*@plain_keys)
+            @nested_keys.each { |path| copy_nested(from: row, to: attrs, from_path: path, to_path: path) }
           end
 
           def to_row(row:, attrs:)
-            row.merge! attrs.slice(*@keys)
-            @keypaths.each { |path| copy_nested(from: attrs, to: row, from_path: path, to_path: path) }
+            row.merge! attrs.slice(*@plain_keys)
+            @nested_keys.each { |path| copy_nested(from: attrs, to: row, from_path: path, to_path: path) }
           end
         end
       end
