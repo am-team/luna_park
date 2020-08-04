c81694f9ec0fc60bc138b5609f2d902b0fe40c58
Captain Philipp <ph-s@mail.ru>
2020-08-04 04:49:42 +0300

    Refactor spec

--- spec/luna_park/tools/environment_spec.rb
+++ spec/luna_park/tools/environment_spec.rb
@@ -4,56 +4,61 @@ require 'luna_park/tools/environment'
 
 module LunaPark
   RSpec.describe Tools::Environment do
-    subject(:app_env) { described_class.new(env, allowed: allowed_envs) }
-
-    let(:env)          { 'test' }
-    let(:allowed_envs) { %i[development test production] }
-
     describe '.new' do
-      it { is_expected.to be_a String }
-      it { is_expected.to be_a Tools::Environment }
-      it { is_expected.to eq   'test' }
+      context 'when given allowed environment name,' do
+        subject(:new) { described_class.new('test', allowed: %i[development test production]) }
 
-      context 'when given not allowed environment name' do
-        let(:env) { 'tesst' }
-        it { expect { app_env }.to raise_error 'Not allowed environment: tesst. Allowed: development, test, production' }
+        it { is_expected.to be_a String }
+        it { is_expected.to be_a Tools::Environment }
+        it { is_expected.to eq   'test' }
+      end
+
+      context 'when given not allowed environment name,' do
+        subject(:new) { described_class.new('tesst', allowed: %i[development test production]) }
+
+        it { expect { new }.to raise_error 'Not allowed environment: tesst. Allowed: development, test, production' }
       end
     end
 
     describe '#inspect' do
-      subject(:inspect) { app_env.inspect }
+      subject(:inspect) { env.inspect }
+      let(:env) { described_class.new('test', allowed: %i[development test production]) }
 
       it { is_expected.to eq '#<LunaPark::Tools::Environment test>' }
     end
 
     describe '#env?' do
-      subject(:env?) { app_env.env?(*checkable_envs) }
+      let(:app_env) { described_class.new('test', allowed: %i[development test production]) }
 
-      let(:checkable_envs) { %i[test development] }
+      context 'when given list contained allowed env,' do
+        subject(:env?) { app_env.env?(:test, :development) }
 
-      it { is_expected.to be true }
+        it { is_expected.to be true }
+      end
 
-      context 'when given list without curent env' do
-        let(:checkable_envs) { %i[development production] }
+      context 'when given list without current env,' do
+        subject(:env?) { app_env.env?(:development, :production) }
 
         it { is_expected.to be false }
       end
 
-      context 'when given list with not allowed env' do
-        let(:checkable_envs) { %i[development proddduction] }
+      context 'when given list with not allowed env,' do
+        subject(:env?) { app_env.env?(:development, :proddduction) }
 
         it { expect { env? }.to raise_error(NoMethodError, /\Aundefined method `proddduction\?'/) }
       end
     end
 
     describe '#==' do
-      context 'when given not String' do
-        it 'compares with Symbol' do
-          expect(app_env == :test).to be true
+      let(:env) { described_class.new('test', allowed: %i[development test production]) }
+
+      context 'when given not String,' do
+        it 'comparable with Symbol' do
+          expect(env == :test).to be true
         end
 
-        it 'compares with any object with #to_s' do
-          expect(app_env == double(to_s: 'test')).to be true
+        it 'comparable with any object with #to_s,' do
+          expect(env == double(to_s: 'test')).to be true
         end
       end
     end
