fcdea02dfac08ed139ad2f92aa360227997f892e
Alexander Kudrin <kudrin.alexander@gmail.com>
2020-12-21 15:24:41 +0100

    Move sentry to LunaPark::Notifiers namespace

--- lib/luna_park/notifiers/sentry.rb
+++ lib/luna_park/notifiers/sentry.rb
@@ -4,37 +4,41 @@ require 'luna_park/extensions/severity_levels'
 require 'luna_park/extensions/injector'
 require 'sentry-raven'
 
-class Sentry
-  include LunaPark::Extensions::SeverityLevels
-  include LunaPark::Extensions::Injector
+module LunaPark
+  module Notifiers
+    class Sentry
+      include LunaPark::Extensions::SeverityLevels
+      include LunaPark::Extensions::Injector
 
-  dependency(:driver) { Raven }
+      dependency(:driver) { Raven }
 
-  def initialize(min_lvl: :debug)
-    self.min_lvl = min_lvl
-  end
+      def initialize(min_lvl: :debug)
+        self.min_lvl = min_lvl
+      end
 
-  def post(msg, lvl: :error, **details)
-    raise ArgumentError, "Undefined severity level `#{lvl}`" unless LEVELS.include? lvl
+      def post(msg, lvl: :error, **details)
+        raise ArgumentError, "Undefined severity level `#{lvl}`" unless LEVELS.include? lvl
 
-    message = wrap msg
-    details = extend details, with: msg
+        message = wrap msg
+        details = extend details, with: msg
 
-    driver.capture_message(message, extra: details, level: lvl)
-  end
+        driver.capture_message(message, extra: details, level: lvl)
+      end
 
-  private
+      private
 
-  def wrap(msg)
-    msg.is_a?(Exception) || msg.is_a?(String) ? msg : msg.inspect
-  end
+      def wrap(msg)
+        msg.is_a?(Exception) || msg.is_a?(String) ? msg : msg.inspect
+      end
 
-  def extend(details, with:)
-    msg = with
-    return details unless msg.respond_to?(:details)
+      def extend(details, with:)
+        msg = with
+        return details unless msg.respond_to?(:details)
 
-    msg.details.merge(details) do |_, msg_value, post_value|
-      { message: msg_value, post: post_value }
+        msg.details.merge(details) do |_, msg_value, post_value|
+          { message: msg_value, post: post_value }
+        end
+      end
     end
   end
 end
--- spec/luna_park/notifiers/sentry_spec.rb
+++ spec/luna_park/notifiers/sentry_spec.rb
@@ -3,173 +3,175 @@
 require 'spec_helper'
 require 'luna_park/notifiers/sentry'
 
-RSpec.describe Sentry do
-  let(:sentry_driver) { class_double Raven, capture_message: true }
-  let(:notifier) { Sentry.new }
+module LunaPark
+  RSpec.describe Notifiers::Sentry do
+    let(:sentry_driver) { class_double Raven, capture_message: true }
+    let(:notifier) { described_class.new }
 
-  before do
-    notifier.dependencies = {
-      driver: -> { sentry_driver }
-    }
-  end
-
-  describe '#post' do
-    context 'when message is not exception' do
-      subject(:post_message) { notifier.post double(inspect: 'Something went wrong') }
-
-      it 'should notify sentry with text of defined message' do
-        expect(sentry_driver).to receive(:capture_message).with(
-          'Something went wrong', extra: {}, level: :error
-        )
-        post_message
-      end
+    before do
+      notifier.dependencies = {
+        driver: -> { sentry_driver }
+      }
     end
 
-    context 'when message is custom error' do
-      class CustomError < RuntimeError; end
-      let(:error) { CustomError.new('Something went wrong. Again.') }
+    describe '#post' do
+      context 'when message is not exception' do
+        subject(:post_message) { notifier.post double(inspect: 'Something went wrong') }
 
-      subject(:post_message) { notifier.post error }
-
-      it 'should notify sentry with CustomError' do
-        expect(sentry_driver).to receive(:capture_message).with(
-          error, extra: {}, level: :error
-        )
-        post_message
-      end
-    end
-
-    context 'when severity level is not defined' do
-      subject(:post_message) { notifier.post('Something went wrong') }
-
-      it 'should notify bugsnag with `error` severity level' do
-        expect(sentry_driver).to receive(:capture_message).with(
-          'Something went wrong', extra: {}, level: :error
-        )
-        post_message
-      end
-    end
-
-    context 'when severity level is debug' do
-      subject(:post_message) { notifier.post('Something went wrong', lvl: :debug) }
-
-      it 'should notify bugsnag with `debug` severity level' do
-        expect(sentry_driver).to receive(:capture_message).with(
-          'Something went wrong', extra: {}, level: :debug
-        )
-        post_message
-      end
-    end
-
-    context 'when severity level is info' do
-      subject(:post_message) { notifier.post('Something went wrong', lvl: :info) }
-
-      it 'should notify bugsnag with `info` severity level' do
-        expect(sentry_driver).to receive(:capture_message).with(
-          'Something went wrong', extra: {}, level: :info
-        )
-        post_message
-      end
-    end
-
-    context 'when severity level is warning' do
-      subject(:post_message) { notifier.post('Something went wrong', lvl: :warning) }
-
-      it 'should notify bugsnag with `warning` severity level' do
-        expect(sentry_driver).to receive(:capture_message).with(
-          'Something went wrong', extra: {}, level: :warning
-        )
-        post_message
-      end
-    end
-
-    context 'when severity level is error' do
-      subject(:post_message) { notifier.post('Something went wrong', lvl: :error) }
-
-      it 'should notify bugsnag with `error` severity level' do
-        expect(sentry_driver).to receive(:capture_message).with(
-          'Something went wrong', extra: {}, level: :error
-        )
-        post_message
-      end
-    end
-
-    context 'when severity level is fatal' do
-      subject(:post_message) { notifier.post('Something went wrong', lvl: :fatal) }
-
-      it 'should notify bugsnag with `error` severity level' do
-        expect(sentry_driver).to receive(:capture_message).with(
-          'Something went wrong', extra: {}, level: :fatal
-        )
-        post_message
-      end
-    end
-
-    context 'when severity level is unknown' do
-      subject(:post_message) { notifier.post('Something went wrong', lvl: :unknown) }
-
-      it 'should notify bugsnag with `error` severity level' do
-        expect(sentry_driver).to receive(:capture_message).with(
-          'Something went wrong', extra: {}, level: :unknown
-        )
-        post_message
-      end
-    end
-
-    context 'when details is not defined' do
-      subject(:post_message) { notifier.post('Something went wrong') }
-
-      it 'should not send empty extra information' do
-        expect(sentry_driver).to receive(:capture_message).with(
-          'Something went wrong', extra: {}, level: :error
-        )
-        post_message
-      end
-    end
-    context 'when details is defined in post' do
-      subject(:post_message) { notifier.post('Something went wrong', something: :wrong) }
-
-      it 'should be send' do
-        expect(sentry_driver).to receive(:capture_message).with(
-          'Something went wrong', extra: { something: :wrong }, level: :error
-        )
-        post_message
-      end
-    end
-
-    context 'when details is defined in message' do
-      let(:msg) { double(details: { answer: 42 }) }
-
-      before do
-        allow(msg).to receive(:inspect).and_return 'Answer to the Ultimate Question of Life, the Universe and Everything'
+        it 'should notify sentry with text of defined message' do
+          expect(sentry_driver).to receive(:capture_message).with(
+            'Something went wrong', extra: {}, level: :error
+          )
+          post_message
+        end
       end
 
-      subject(:post_message) { notifier.post(msg) }
+      context 'when message is custom error' do
+        class CustomError < RuntimeError; end
+        let(:error) { CustomError.new('Something went wrong. Again.') }
 
-      it 'should be send with' do
-        expect(sentry_driver).to receive(:capture_message).with(
-          'Answer to the Ultimate Question of Life, the Universe and Everything',
-          extra: { answer: 42 }, level: :error
-        )
-        post_message
-      end
-    end
+        subject(:post_message) { notifier.post error }
 
-    context 'when details is defined in error and in post with same key' do
-      let(:msg) { double(details: { answer: 42 }) }
-
-      before do
-        allow(msg).to receive(:inspect).and_return 'Answer to the Ultimate Question of Life, the Universe and Everything'
+        it 'should notify sentry with CustomError' do
+          expect(sentry_driver).to receive(:capture_message).with(
+            error, extra: {}, level: :error
+          )
+          post_message
+        end
       end
 
-      subject(:post_message) { notifier.post(msg, answer: 41) }
+      context 'when severity level is not defined' do
+        subject(:post_message) { notifier.post('Something went wrong') }
 
-      it 'should be send with' do
-        expect(sentry_driver).to receive(:capture_message).with(
-          'Answer to the Ultimate Question of Life, the Universe and Everything',
-          extra: { answer: { message: 42, post: 41 } }, level: :error
-        )
-        post_message
+        it 'should notify bugsnag with `error` severity level' do
+          expect(sentry_driver).to receive(:capture_message).with(
+            'Something went wrong', extra: {}, level: :error
+          )
+          post_message
+        end
+      end
+
+      context 'when severity level is debug' do
+        subject(:post_message) { notifier.post('Something went wrong', lvl: :debug) }
+
+        it 'should notify bugsnag with `debug` severity level' do
+          expect(sentry_driver).to receive(:capture_message).with(
+            'Something went wrong', extra: {}, level: :debug
+          )
+          post_message
+        end
+      end
+
+      context 'when severity level is info' do
+        subject(:post_message) { notifier.post('Something went wrong', lvl: :info) }
+
+        it 'should notify bugsnag with `info` severity level' do
+          expect(sentry_driver).to receive(:capture_message).with(
+            'Something went wrong', extra: {}, level: :info
+          )
+          post_message
+        end
+      end
+
+      context 'when severity level is warning' do
+        subject(:post_message) { notifier.post('Something went wrong', lvl: :warning) }
+
+        it 'should notify bugsnag with `warning` severity level' do
+          expect(sentry_driver).to receive(:capture_message).with(
+            'Something went wrong', extra: {}, level: :warning
+          )
+          post_message
+        end
+      end
+
+      context 'when severity level is error' do
+        subject(:post_message) { notifier.post('Something went wrong', lvl: :error) }
+
+        it 'should notify bugsnag with `error` severity level' do
+          expect(sentry_driver).to receive(:capture_message).with(
+            'Something went wrong', extra: {}, level: :error
+          )
+          post_message
+        end
+      end
+
+      context 'when severity level is fatal' do
+        subject(:post_message) { notifier.post('Something went wrong', lvl: :fatal) }
+
+        it 'should notify bugsnag with `error` severity level' do
+          expect(sentry_driver).to receive(:capture_message).with(
+            'Something went wrong', extra: {}, level: :fatal
+          )
+          post_message
+        end
+      end
+
+      context 'when severity level is unknown' do
+        subject(:post_message) { notifier.post('Something went wrong', lvl: :unknown) }
+
+        it 'should notify bugsnag with `error` severity level' do
+          expect(sentry_driver).to receive(:capture_message).with(
+            'Something went wrong', extra: {}, level: :unknown
+          )
+          post_message
+        end
+      end
+
+      context 'when details is not defined' do
+        subject(:post_message) { notifier.post('Something went wrong') }
+
+        it 'should not send empty extra information' do
+          expect(sentry_driver).to receive(:capture_message).with(
+            'Something went wrong', extra: {}, level: :error
+          )
+          post_message
+        end
+      end
+      context 'when details is defined in post' do
+        subject(:post_message) { notifier.post('Something went wrong', something: :wrong) }
+
+        it 'should be send' do
+          expect(sentry_driver).to receive(:capture_message).with(
+            'Something went wrong', extra: { something: :wrong }, level: :error
+          )
+          post_message
+        end
+      end
+
+      context 'when details is defined in message' do
+        let(:msg) { double(details: { answer: 42 }) }
+
+        before do
+          allow(msg).to receive(:inspect).and_return 'Answer to the Ultimate Question of Life, the Universe and Everything'
+        end
+
+        subject(:post_message) { notifier.post(msg) }
+
+        it 'should be send with' do
+          expect(sentry_driver).to receive(:capture_message).with(
+            'Answer to the Ultimate Question of Life, the Universe and Everything',
+            extra: { answer: 42 }, level: :error
+          )
+          post_message
+        end
+      end
+
+      context 'when details is defined in error and in post with same key' do
+        let(:msg) { double(details: { answer: 42 }) }
+
+        before do
+          allow(msg).to receive(:inspect).and_return 'Answer to the Ultimate Question of Life, the Universe and Everything'
+        end
+
+        subject(:post_message) { notifier.post(msg, answer: 41) }
+
+        it 'should be send with' do
+          expect(sentry_driver).to receive(:capture_message).with(
+            'Answer to the Ultimate Question of Life, the Universe and Everything',
+            extra: { answer: { message: 42, post: 41 } }, level: :error
+          )
+          post_message
+        end
       end
     end
   end
