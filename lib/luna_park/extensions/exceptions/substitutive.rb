b7d53a4f957e70b184554cb5c4e7a93e03abbbdf
Captain Philipp <ph-s@mail.ru>
2021-08-30 17:50:59 +0300

    Rm Substitutive changes

--- lib/luna_park/extensions/exceptions/substitutive.rb
+++ lib/luna_park/extensions/exceptions/substitutive.rb
@@ -4,32 +4,6 @@ module LunaPark
   module Extensions
     module Exceptions
       # class-level mixin
-      #
-      # @example Bad case (without Substitutive)
-      #   class CustomException < StandardError; end
-      #
-      #   begin
-      #     use_exceptional_gem!
-      #   rescue ExceptionFromGem => ex
-      #     raise CustomException
-      #   end
-      #
-      #   # Raised exception has backtrace started from `raise CustomException`
-      #   # So it's not containing the origin backtrace, that can be very painfull for debug
-      #
-      # @example Good case (with Substitutive)
-      #   class CustomException < StandardError
-      #     extend LunaPark::Extensions::Exceptions::Substitutive
-      #   end
-      #
-      #   begin
-      #     use_exceptional_gem!
-      #   rescue ExceptionFromGem => ex
-      #     raise CustomException.substitute(ex, "Oh! That's bad!", custom_data: ex.detail)
-      #   end
-      #
-      #   # Raised exception has backtrace started from origin backtrace of ExceptionFromGem
-      #   # So you can easily find out where was the exception started
       module Substitutive
         def self.extended(base)
           base.extend  ClassMethods
@@ -37,34 +11,69 @@ module LunaPark
         end
 
         module ClassMethods
-          def substitute(origin, *args, **opts)
-            instance = __initializer_has_named_args? ? new(*args, **opts) : new(*args)
-            instance.substitute!(origin)
-          end
-
-          private
-
-          def __initializer_has_named_args?
-            return @__initializer_has_named_args unless @__initializer_has_named_args.nil?
-
-            signature      = instance_method(:initialize).parameters
-            arg_types      = signature.map!(&:first)
-            has_named_args = arg_types.any? { |arg_type| %i[keyreq key keyrest].include?(arg_type) }
-
-            @__initializer_has_named_args = has_named_args
+          # Substitute original exception with save original backtrace.
+          #
+          # @example bad case
+          #   class MyException < StandardError
+          #     extend LunaPark::Extensions::Exceptions::Substitutive
+          #   end
+          #
+          #     call_exceptional_lib!
+          #   rescue ExceptionalLib::SomeException => e
+          #     raise MyException # => raised MyException with backtrace started from `raise MyException`
+          #                       #      and not contained origin exception backtrace
+          #                       #      that can be very painfull for debug
+          #   end
+          #
+          # @example resolve
+          #   begin
+          #     call_exceptional_lib!
+          #   rescue ExceptionalLib::SomeException => e
+          #     raise MyException.substitute(e) # => raised MyException with backtrace started
+          #                                     #      from library `raise ExceptionalLib::SomeException`
+          #                                     #      so you can easily find out where exception starts
+          #   end
+          def substitute(origin)
+            new = new(origin.message)
+            new.backtrace = origin.backtrace
+            new.origin    = origin
+            new
           end
         end
 
         module InstanceMethods
-          attr_reader :origin
-
-          def substitute!(origin)
-            @origin = origin
-            self
-          end
+          attr_accessor :origin
+          attr_writer :backtrace
 
           def backtrace
-            super || origin&.backtrace
+            super || @backtrace
+          end
+
+          # Cover up trace for current exception
+          #
+          # @example bad case
+          #   begin
+          #     call_exceptional_lib!
+          #   rescue ExceptionalLib::SomeException => e
+          #     send_alert_to_developer
+          #     raise e # => raised `ExceptionalLib::SomeException` with backtrace started
+          #             #      from current line and not contained origin exception backtrace
+          #             #      that can be very painful for debug
+          #   end
+          #
+          # @example resolve
+          #     begin
+          #       call_exceptional_lib!
+          #     rescue ExceptionalLib::SomeException => e
+          #       send_alert_to_developer
+          #       raise e.cover_up_backtrace # => raised `ExceptionalLib::SomeException` with original backtrace
+          #                                  #       so you can easily find out where exception starts
+          #     end
+          def cover_up_backtrace
+            new = dup
+            new.backtrace = backtrace
+            new.origin    = self
+            new
           end
         end
       end
