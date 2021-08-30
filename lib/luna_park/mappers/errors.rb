525da2d45ba747d5e38cd0a06706d6d0c4868955
Captain Philipp <ph-s@mail.ru>
2021-08-30 18:03:57 +0300

    Renew exception NotArray

--- lib/luna_park/mappers/errors.rb
+++ lib/luna_park/mappers/errors.rb
@@ -5,10 +5,8 @@ require 'luna_park/errors'
 module LunaPark
   module Mappers
     module Errors
-      class NotArray < ArgumentError
-        def initialize(message = nil, input:, **_)
-          super message || "input MUST be an Array, but given #{input.class} `#{input.inspect}`"
-        end
+      class NotArray < LunaPark::Errors::System
+        message { |d| "input MUST be an Array, but given #{d[:input].class} `#{d[:input].inspect}`" }
       end
     end
   end
