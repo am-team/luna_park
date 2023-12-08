991915dac5e36ce0c7ab8cc6e6cd27852e40017b
IAmPhilipp <ph-s@mail.ru>
2023-12-08 14:19:18 +0100

    Changelog, Version, Rcop

--- lib/luna_park/extensions/repositories/postgres/delete.rb
+++ lib/luna_park/extensions/repositories/postgres/delete.rb
@@ -6,11 +6,11 @@ module LunaPark
       module Postgres
         module Delete
           def delete(input, **scope_opts)
-            case input
-            when self.class.entity_class then pk_value = input.public_send(primary_key)
-            when Hash                    then pk_value = input[primary_key]
-            else pk_value = input
-            end
+            pk_value = case input
+                       when self.class.entity_class then input.public_send(primary_key)
+                       when Hash                    then input[primary_key]
+                       else input
+                       end
 
             raise ArgumentError, "primary key '#{primary_key}' value can't be nil" if pk_value.nil?
 
