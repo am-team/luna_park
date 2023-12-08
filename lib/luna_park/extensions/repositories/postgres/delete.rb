a43e9c0ee2358d006d16a84e231eab4794a9ab69
IAmPhilipp <ph-s@mail.ru>
2023-12-08 14:14:01 +0100

    #delete cen receive Entity, Hash

--- lib/luna_park/extensions/repositories/postgres/delete.rb
+++ lib/luna_park/extensions/repositories/postgres/delete.rb
@@ -5,7 +5,15 @@ module LunaPark
     module Repositories
       module Postgres
         module Delete
-          def delete(pk_value, **scope_opts)
+          def delete(input, **scope_opts)
+            case input
+            when self.class.entity_class then pk_value = input.public_send(primary_key)
+            when Hash                    then pk_value = input[primary_key]
+            else pk_value = input
+            end
+
+            raise ArgumentError, "primary key '#{primary_key}' value can't be nil" if pk_value.nil?
+
             scoped(**scope_opts).where(primary_key => pk_value).delete.positive?
           end
         end
--- spec/luna_park/extensions/repositories/postgres/delete_spec.rb
+++ spec/luna_park/extensions/repositories/postgres/delete_spec.rb
@@ -12,25 +12,72 @@ RSpec.describe LunaPark::Extensions::Repositories::Postgres::Delete do
       attr_accessor :dataset
 
       primary_key :uid
+      entity OpenStruct
     end
   end
   let(:fake_repo) { fake_repo_klass.new }
+  let(:uuid) { SecureRandom.uuid }
 
   describe '#delete' do
-    subject(:delete) { fake_repo.delete(SecureRandom.uuid) }
-
     before do
       fake_repo.dataset = double
       allow(fake_repo.dataset).to receive_message_chain(:where, :delete).and_return 1
     end
 
-    it 'returns boolean' do
-      expect(delete).to be true
+    context 'when pk given' do
+      subject(:delete) { fake_repo.delete(uuid) }
+
+      it 'returns boolean' do
+        expect(delete).to be true
+      end
+
+      it 'sends delete to db' do
+        expect(fake_repo.dataset).to receive(:where).with(uid: uuid)
+        delete
+      end
     end
 
-    it 'sends delete to db' do
-      expect(fake_repo.dataset).to receive_message_chain(:where, :delete)
-      delete
+    context 'when expected entity given' do
+      subject(:delete) { fake_repo.delete(fake_repo_klass.entity_class.new(uid: uuid)) }
+
+      it 'sends delete to db' do
+        expect(fake_repo.dataset).to receive(:where).with(uid: uuid)
+        delete
+      end
+    end
+
+    context 'when Hash given' do
+      subject(:delete) { fake_repo.delete({ uid: uuid }) }
+
+      it 'sends delete to db' do
+        expect(fake_repo.dataset).to receive(:where).with(uid: uuid)
+        delete
+      end
+    end
+
+    context 'when extractable pk is nil' do
+      subject(:delete) { fake_repo.delete({ uid: nil }) }
+
+      it 'raises meaningfull error' do
+        expect { delete }.to raise_error ArgumentError, "primary key 'uid' value can't be nil"
+      end
+    end
+
+    context 'when pk is nil' do
+      subject(:delete) { fake_repo.delete(nil) }
+
+      it 'raises meaningfull error' do
+        expect { delete }.to raise_error ArgumentError, "primary key 'uid' value can't be nil"
+      end
+    end
+
+    context 'when any other type given' do
+      subject(:delete) { fake_repo.delete(123) }
+
+      it 'sends delete to db' do
+        expect(fake_repo.dataset).to receive(:where).with(uid: 123)
+        delete
+      end
     end
   end
 end
